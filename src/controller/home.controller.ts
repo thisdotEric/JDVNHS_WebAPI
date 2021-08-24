85d83d3b64ee840d81e192b3e62c886d4f00fab0
thisdotEric <siguenzajohneric@gmail.com>
2021-08-24 10:07:29 +0800

    Fixed: Controller spelling

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -2,7 +2,7 @@ import { controller, httpGet } from 'inversify-express-utils';
 import { Request, Response } from 'express';
 
 @controller('/')
-export class HomeContoller {
+export class HomeController {
     @httpGet('/')
     async index(req: Request, res: Response) {
         res.status(200).send('JDVNHS WebAPI');

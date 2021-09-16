f06c86f2d236325cc0f17f7232d356d4e53780fc
thisdotEric <siguenzajohneric@gmail.com>
2021-09-16 18:12:40 +0800

    Added student controller class

---
+++ src/controller/student.controller.ts
@@ -0,0 +1,34 @@
+import {
+    controller,
+    httpGet,
+    BaseHttpController,
+    request,
+    response,
+} from 'inversify-express-utils';
+import { inject } from 'inversify';
+import { Request, Response } from 'express';
+import JsonResponse from '../utils/JsonResponse';
+import StudentService from '../services/student.service';
+import TYPES from '../ioc/binding-types';
+
+@controller('/student')
+class StudentController extends BaseHttpController {
+    constructor(
+        @inject(TYPES.StudentService)
+        private readonly studentService: StudentService
+    ) {
+        super();
+    }
+
+    @httpGet('/:lrn')
+    async all(@request() req: Request, @response() res: Response) {
+        const student = await this.studentService.getStudentByLRN(
+            req.params.lrn
+        );
+
+        const response = JsonResponse.success(student, 200);
+        res.status(response.statusCode).send(response);
+    }
+}
+
+export default StudentController;

b0d71990ee614a22edd371ff15047f5620b60932
thisdotEric <siguenzajohneric@gmail.com>
2021-09-18 01:05:20 +0800

    Added subject schedule seed data

---
+++ src/database/knexQueryBuilder/seeds/004_subject_schedule.ts
@@ -0,0 +1,23 @@
+import * as Knex from 'knex';
+import { DbConstants } from '../../../constant/db.constants';
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex(DbConstants.SCHEDULE_TABLE).del();
+
+    // Inserts seed entries
+    await knex(DbConstants.SCHEDULE_TABLE).insert([
+        {
+            subject_id: 'MMW1',
+            meeting_day: 'Wednesday',
+            start_time: '10:00',
+            end_time: '13:00',
+        },
+        {
+            subject_id: 'MMW1',
+            meeting_day: 'Monday',
+            start_time: '10:00',
+            end_time: '11:00',
+        },
+    ]);
+}

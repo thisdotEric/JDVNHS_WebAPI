ab05f6ff09d29a051df97b906d830ee1e408deb4
thisdotEric <siguenzajohneric@gmail.com>
2021-09-18 01:03:07 +0800

    Altered subjects schedule schema

--- src/database/knexQueryBuilder/migrations/20210917105241_create_subject_schedule.ts
+++ src/database/knexQueryBuilder/migrations/20210917105241_create_subject_schedule.ts
@@ -3,10 +3,24 @@ import { DbConstants } from '../../../constant/db.constants';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable(DbConstants.SCHEDULE_TABLE, table => {
-        table.string('schedule_id').notNullable().primary().unique();
-        table.string('day').notNullable();
+        table.string('subject_id').notNullable();
+        table
+            .enum('meeting_day', [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+            ])
+            .defaultTo('Monday');
         table.string('start_time').notNullable().defaultTo('08:00');
         table.string('end_time').notNullable().defaultTo('08:00');
+
+        table
+            .foreign('subject_id')
+            .references('subject_id')
+            .inTable(DbConstants.SUBJECT_TABLE);
     });
 }
 

a08783e17449a0feb45b6579abfc5347956d5c86
thisdotEric <siguenzajohneric@gmail.com>
2021-10-04 13:46:35 +0800

    Setup CORS, allows JDVNHS localhost website to connect

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import container from './ioc/ioc-container';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import express from 'express';
 import exceptionsMiddleware from './middleware/exceptions.middleware';
+import cors from './utils/cors';
 
 // Controllers
 import './controller/home.controller';
@@ -17,6 +18,7 @@ server.setErrorConfig(app => {
 });
 
 server.setConfig(app => {
+    app.use(cors);
     app.use(express.json());
 });
 
---
+++ src/utils/cors.ts
@@ -0,0 +1,11 @@
+import cors, { CorsOptions } from 'cors';
+
+const corsOptions: CorsOptions = {
+    origin: [
+        `${process.env.JDVNHS_WEBSITE_LOCALHOST_URL}`,
+        `${process.env.JDVNHS_WEBSITE_REMOTE_URL}`,
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+};
+
+export default cors(corsOptions);

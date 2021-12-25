8922d5657cef383114f3ebda430edbc3f3bff5d6
thisdotEric <siguenzajohneric@gmail.com>
2021-12-25 17:59:49 +0800

    Fixed breaking changes from inversify library

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 if (process.env.NODE_ENV != 'production') require('dotenv').config();
 
 import 'reflect-metadata';
-import container from './ioc/ioc-container';
+import bindings from './ioc/ioc-container';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import express from 'express';
 import exceptionsMiddleware from './middleware/exceptions.middleware';
 import cors from './utils/cors';
+import { Container } from 'inversify';
 
 // Controllers
 import './controller/home.controller';
@@ -13,23 +14,30 @@ import './controller/student.controller';
 import './controller/subject.controller';
 import './controller/authentication.controller';
 
-const server = new InversifyExpressServer(container, null, { rootPath: '/v1' });
+(async () => {
+  let container = new Container();
+  await container.loadAsync(bindings);
 
-// Catch all exceptions thrown from any part of the application
-server.setErrorConfig(app => {
-  app.use(exceptionsMiddleware);
-});
+  let server = new InversifyExpressServer(container, null, {
+    rootPath: '/v1',
+  });
 
-// Add all the necessary middlewares
-server.setConfig(app => {
-  app.use(cors);
-  app.use(express.json());
-});
+  // Catch all exceptions thrown from any part of the application
+  server.setErrorConfig(app => {
+    app.use(exceptionsMiddleware);
+  });
 
-const app = server.build();
+  // Add all the necessary middlewares
+  server.setConfig(app => {
+    app.use(cors);
+    app.use(express.json());
+  });
 
-const port = parseInt(<string>process.env.PORT, 10) || 4000;
+  const app = server.build();
 
-export default app.listen(port, () => {
-  console.log(`Server running in port ${port}`);
-});
+  const port = parseInt(<string>process.env.PORT, 10) || 4000;
+
+  app.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+  });
+})();

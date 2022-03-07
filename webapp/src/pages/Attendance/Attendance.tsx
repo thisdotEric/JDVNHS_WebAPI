26f27fdb676096d53ed08f136fc6bb6e0760b758
thisdotEric <siguenzajohneric@gmail.com>
2022-03-07 20:55:47 +0800

    Fixed attendance table bug

--- webapp/src/pages/Attendance/Attendance.tsx
+++ webapp/src/pages/Attendance/Attendance.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useEffect, useState, useContext } from 'react';
 import './Attendance.scss';
 import { axios } from '../../utils';
-import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import { SubjectContext } from '../../context';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { attendanceColumns } from './columns';
 
 interface AttendanceProps {}
 
@@ -20,77 +21,89 @@ interface Attendance {
   status: AttendanceStatus;
 }
 
+interface UpdateAttendanceProp {
+  table: any;
+  updatedAttendance: AttendanceStatus;
+  updateStudentAttendance: (
+    table: any,
+    updatedAttendance: AttendanceStatus,
+  ) => Promise<void>;
+}
+
+const UpdateAttendance = ({
+  table,
+  updatedAttendance,
+  updateStudentAttendance,
+}: UpdateAttendanceProp) => {
+  return (
+    <button
+      onClick={async () => {
+        await updateStudentAttendance(table, updatedAttendance);
+      }}
+    >
+      {updatedAttendance}
+    </button>
+  );
+};
+
 const Attendance: FC<AttendanceProps> = ({}: AttendanceProps) => {
   const [attendanceList, setAttendanceList] = useState<
     Attendance & { lecture_id: number }[]
   >();
   const selectedSubject = useContext(SubjectContext);
   const [attendanceDate, setAttendanceDate] = useState<Date>(new Date());
-  // const [validLectureDates, setValidLectureDates] =
-  //   useState<{ lecture_date: Date }[]>();
+  const [showMainAttendanceTable, setAttendanceTable] = useState<boolean>(true);
+  const [attendanceUpdate, setAttendanceUpdate] = useState<number>(0);
 
   const [columns] = useState([
+    ...attendanceColumns,
     {
-      field: 'LRN',
-      headerName: 'LRN',
-    },
-    {
-      field: 'first_name',
-      headerName: 'First Name',
-    },
-    {
-      field: 'middle_name',
-      headerName: 'Middle Name',
-    },
-    {
-      field: 'last_name',
-      headerName: 'Last Name',
-    },
-    {
-      field: 'status',
-      headerName: 'Attendance',
-    },
-    {
-      field: 'lecture_id',
-      headerName: 'Lecture_id',
-      hide: true,
-      default: 13,
+      headerName: 'Action',
+      cellRendererFramework: (params: any) => (
+        <UpdateAttendance
+          table={params}
+          updateStudentAttendance={updateStudentAttendance}
+          updatedAttendance="present"
+        />
+      ),
     },
     {
       headerName: 'Action',
-      cellRendererFramework: (params: any) => {
-        const [status] = useState<AttendanceStatus[]>([
-          'present',
-          'excused',
-          'absent',
-        ]);
-
-        return (
-          <div>
-            {status.map(stat => (
-              <button
-                key={stat}
-                onClick={async () => {
-                  const LRN = params.data.LRN;
-                  const lecture_id = params.data.lecture_id;
-
-                  await axios.patch(
-                    `subject/${selectedSubject}/${lecture_id}/attendance`,
-                    {
-                      LRN,
-                      newStatus: stat,
-                    },
-                  );
-                }}
-              >
-                {stat}
-              </button>
-            ))}
-          </div>
-        );
-      },
+      cellRendererFramework: (params: any) => (
+        <UpdateAttendance
+          table={params}
+          updateStudentAttendance={updateStudentAttendance}
+          updatedAttendance="absent"
+        />
+      ),
+    },
+    {
+      headerName: 'Action',
+      cellRendererFramework: (params: any) => (
+        <UpdateAttendance
+          table={params}
+          updateStudentAttendance={updateStudentAttendance}
+          updatedAttendance="excused"
+        />
+      ),
     },
   ]);
+
+  const updateStudentAttendance = async (
+    table: any,
+    updatedAttendance: AttendanceStatus,
+  ) => {
+    const LRN = table.data.LRN;
+    const lecture_id = table.data.lecture_id;
+
+    await axios.patch(`subject/${selectedSubject}/${lecture_id}/attendance`, {
+      LRN,
+      newStatus: updatedAttendance,
+    });
+
+    // Set new value to re-fetch updated attendance list
+    setAttendanceUpdate(Math.random());
+  };
 
   const fetchStudentsAttendance = () => {
     // Format date in accordance to servers expected date (yyyy-MM-dd)
@@ -102,27 +115,27 @@ const Attendance: FC<AttendanceProps> = ({}: AttendanceProps) => {
       })
       .replaceAll('/', '-');
 
-    axios
-      .get(`subject/${selectedSubject}/attendance/${date}`)
-      .then(({ data }) => {
-        setAttendanceList(() => {
-          return data.data.attendance.map((at: any) => ({
-            ...at,
-            lecture_id: data.data.lecture_id,
-          }));
+    if (!selectedSubject) {
+      console.log('Subject is still undefined');
+    } else
+      axios
+        .get(`subject/${selectedSubject}/attendance/${date}`)
+        .then(({ data }) => {
+          setAttendanceList(() => {
+            return data.data.attendance.map((at: any) => ({
+              ...at,
+              lecture_id: data.data.lecture_id,
+            }));
+          });
+        })
+        .catch(err => {
+          console.log('Not Found');
         });
-      });
   };
 
   useEffect(() => {
     fetchStudentsAttendance();
-
-    // axios
-    //   .get(`subject/${selectedSubject}/lecture-dates?teacher=${user?.user_id}`)
-    //   .then(({ data }) => {
-    //     setValidLectureDates(data.data);
-    //   });
-  }, [selectedSubject, attendanceDate]);
+  }, [selectedSubject, attendanceDate, attendanceUpdate]);
 
   useEffect(() => {
     fetchStudentsAttendance();
@@ -136,30 +149,41 @@ const Attendance: FC<AttendanceProps> = ({}: AttendanceProps) => {
         calendarType="Hebrew"
         tileDisabled={({ activeStartDate, date, view }) => date.getDay() === 0}
       />
-      <div
-        className="ag-theme-balham"
-        id="student-table"
-        style={{
-          height: '550px',
+      <button
+        onClick={() => {
+          setAttendanceTable(!showMainAttendanceTable);
         }}
       >
-        <AgGridReact
-          rowData={attendanceList}
-          columnDefs={columns}
-          pagination={true}
-          rowSelection={'single'}
-          enableCellChangeFlash={true}
-          pinnedTopRowData={[]}
-          pinnedBottomRowData={[]}
-          defaultColDef={{
-            sortable: true,
-            flex: 1,
-            minWidth: 100,
-            filter: true,
-            resizable: true,
+        Create new attendance
+      </button>
+      {showMainAttendanceTable ? (
+        <div
+          className="ag-theme-balham"
+          id="student-table"
+          style={{
+            height: '550px',
           }}
-        ></AgGridReact>
-      </div>
+        >
+          <AgGridReact
+            rowData={attendanceList}
+            columnDefs={columns}
+            pagination={true}
+            rowSelection={'single'}
+            enableCellChangeFlash={true}
+            pinnedTopRowData={[]}
+            pinnedBottomRowData={[]}
+            defaultColDef={{
+              sortable: true,
+              flex: 1,
+              minWidth: 100,
+              filter: true,
+              resizable: true,
+            }}
+          ></AgGridReact>
+        </div>
+      ) : (
+        <p>John</p>
+      )}
     </>
   );
 };
---
+++ webapp/src/pages/Attendance/columns.ts
@@ -0,0 +1,29 @@
+const attendanceColumns = [
+  {
+    field: 'LRN',
+    headerName: 'LRN',
+  },
+  {
+    field: 'first_name',
+    headerName: 'First Name',
+  },
+  {
+    field: 'middle_name',
+    headerName: 'Middle Name',
+  },
+  {
+    field: 'last_name',
+    headerName: 'Last Name',
+  },
+  {
+    field: 'status',
+    headerName: 'Attendance',
+  },
+  {
+    field: 'lecture_id',
+    headerName: 'Lecture_id',
+    hide: true,
+  },
+];
+
+export { attendanceColumns };

38c1a77b26e196f09b2fd6859f179fbfffc126ef
thisdotEric <siguenzajohneric@gmail.com>
2022-03-16 00:21:50 +0800

    Updated scores pages

--- webapp/src/pages/Scores/Scores.tsx
+++ webapp/src/pages/Scores/Scores.tsx
@@ -2,6 +2,10 @@ import React, { FC, useContext, useEffect, useState } from 'react';
 import './Scores.scss';
 import { axios } from '../../utils';
 import { SubjectContext } from '../../../src/context';
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import {} from '../../constants/';
 
 interface ScoresProps {}
 
@@ -19,19 +23,67 @@ interface Scores {
   score: number;
 }
 
-// assessment_id |    date    | subject_id | items | component
+type LearningComponent = 'WW' | 'PT' | 'QA';
 
 interface Assessments {
   assessment_id: number;
   date: Date;
   subject_id: string;
   items: number;
-  component: 'WW' | 'PT' | 'QA';
+  component: LearningComponent;
+}
+
+const scoresColumns = [
+  {
+    field: 'LRN',
+    headerName: 'LRN',
+  },
+  {
+    field: 'score',
+    headerName: 'Score',
+  },
+  {
+    field: 'score_id',
+    headerName: 'Score_id',
+    hide: true,
+  },
+];
+
+function toLongLearningComponentName(
+  learningComponent: LearningComponent,
+): string {
+  switch (learningComponent) {
+    case 'PT':
+      return 'Performance Task (PT)';
+    case 'QA':
+      return 'Quarterly Assessment (QA)';
+    case 'WW':
+      return 'Written Work (WW)';
+    default:
+      return '';
+  }
+}
+
+function shortenDate(date: Date) {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return new Date(date).toLocaleDateString(undefined, {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 }
 
 const Scores: FC<ScoresProps> = ({}: ScoresProps) => {
-  const [classScores, setScores] = useState<ClassScores>();
+  const [classScores, setScores] = useState<Scores[]>();
   const [assessments, setAssessments] = useState<Assessments[]>();
+  const [showTable, setShowTable] = useState<boolean>(false);
 
   const selectedSubject = useContext(SubjectContext);
 
@@ -40,12 +92,10 @@ const Scores: FC<ScoresProps> = ({}: ScoresProps) => {
       `subject/${selectedSubject}/scores/${assessment_id}`,
     );
 
-    console.log(res.data);
+    setScores(res.data.data);
   };
 
   useEffect(() => {
-    console.log(selectedSubject);
-
     axios.get(`subject/${selectedSubject}/assessments/all`).then(({ data }) => {
       setAssessments(data.data);
     });
@@ -53,21 +103,53 @@ const Scores: FC<ScoresProps> = ({}: ScoresProps) => {
 
   return (
     <div className="scores">
-      {assessments &&
-        assessments.map(assessments => {
-          return (
-            <div
-              key={assessments.assessment_id}
-              onClick={async () => {
-                await fetchAssessmentScores(assessments.assessment_id);
-              }}
-            >
-              <p>{assessments.date}</p>
-              <p>{assessments.items}</p>
-              <p>{assessments.component}</p>
-            </div>
-          );
-        })}
+      <div className="scores-accordion">
+        {assessments &&
+          assessments.map(assessments => {
+            return (
+              <div key={assessments.assessment_id}>
+                <div
+                  className="header"
+                  onClick={async () => {
+                    await fetchAssessmentScores(assessments.assessment_id);
+                    setShowTable(!showTable);
+                  }}
+                >
+                  <p>{shortenDate(assessments.date)}</p>
+                  <p>Items: {assessments.items}</p>
+                  <p>
+                    Component Type:{' '}
+                    {toLongLearningComponentName(assessments.component)}
+                  </p>
+                </div>
+                {showTable && (
+                  <div
+                    className="ag-theme-balham"
+                    id="student-table"
+                    style={{
+                      height: '550px',
+                    }}
+                  >
+                    <AgGridReact
+                      rowData={classScores}
+                      pagination={true}
+                      columnDefs={scoresColumns}
+                      rowSelection={'single'}
+                      enableCellChangeFlash={true}
+                      defaultColDef={{
+                        sortable: true,
+                        flex: 1,
+                        minWidth: 100,
+                        filter: true,
+                        resizable: true,
+                      }}
+                    ></AgGridReact>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };

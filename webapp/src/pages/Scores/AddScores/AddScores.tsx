ae4624ddf189c0bbb8e0beddd91b52ab374e4309
thisdotEric <siguenzajohneric@gmail.com>
2022-03-23 15:51:01 +0800

    Started implementing adding of assessment scores

--- webapp/src/pages/Scores/AddScores/AddScores.tsx
+++ webapp/src/pages/Scores/AddScores/AddScores.tsx
@@ -1,12 +1,95 @@
 import { AgGridReact } from 'ag-grid-react';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useContext, useState } from 'react';
 import './AddScores.scss';
+import { axios } from '../../../utils';
+import { SubjectContext } from '../../../context';
 
 interface AddScoresProps {}
 
+interface Student {
+  user_id: string;
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  score: number;
+}
+
+interface NewScore {
+  LRN: string;
+  score: number;
+}
+
 const AddScores: FC<AddScoresProps> = ({}: AddScoresProps) => {
+  const selectedSubject = useContext(SubjectContext);
+  const [students, setStudents] = useState<Student[]>([]);
+
+  const [scoreColumns] = useState([
+    {
+      field: 'user_id',
+      headerName: 'LRN',
+    },
+    {
+      field: 'last_name',
+      headerName: 'Last Name',
+    },
+    {
+      field: 'first_name',
+      headerName: 'First Name',
+    },
+    {
+      field: 'middle_name',
+      headerName: 'Middle Name',
+    },
+    {
+      field: 'score',
+      headerName: 'Score',
+      editable: true,
+      onCellValueChanged: (grid: any) => {
+        let score: number;
+
+        score = parseInt(grid.node.data.score, 10);
+
+        // if (Number.isNaN(score)) console.log('Invalid NUmber');
+        // else console.log(score);
+        console.log(grid.node.id);
+
+        const currIndex = parseInt(grid.node.id, 10);
+
+        setStudents(old => {
+          return old.map((s, index) => {
+            if (index == currIndex) s.score = score;
+
+            return s;
+          });
+        });
+      },
+    },
+  ]);
+
+  useEffect(() => {
+    axios.get(`subject/${selectedSubject}/students`).then(({ data }) => {
+      setStudents(
+        data.data.map((s: any) => ({
+          user_id: s.user_id,
+          first_name: s.first_name,
+          middle_name: s.middle_name,
+          last_name: s.last_name,
+          score: 0,
+        })),
+      );
+    });
+  }, []);
+
   return (
     <div>
+      <button
+        onClick={() => {
+          console.table(students);
+        }}
+      >
+        Save
+      </button>
+
       <div
         className="ag-theme-balham"
         id="student-table"
@@ -15,9 +98,9 @@ const AddScores: FC<AddScoresProps> = ({}: AddScoresProps) => {
         }}
       >
         <AgGridReact
-          // rowData={classScores}
+          rowData={students}
           pagination={true}
-          // columnDefs={scoreColumns}
+          columnDefs={scoreColumns}
           rowSelection={'single'}
           enableCellChangeFlash={true}
           defaultColDef={{

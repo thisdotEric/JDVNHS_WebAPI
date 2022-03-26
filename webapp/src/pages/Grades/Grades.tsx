ceed8a6d5d87e583ac31a407225eaefa7aebb486
thisdotEric <siguenzajohneric@gmail.com>
2022-03-26 15:30:27 +0800

    Added Grades pages

---
+++ webapp/src/pages/Grades/Grades.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './Grades.scss';
+
+interface GradesProps {}
+
+const Grades: FC<GradesProps> = ({}: GradesProps) => {
+  return (
+    <div>
+      <p>Grades</p>
+    </div>
+  );
+};
+
+export default Grades;

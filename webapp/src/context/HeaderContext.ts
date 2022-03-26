cb190dae49235e5cf50baed5327a4e0a5134af82
thisdotEric <siguenzajohneric@gmail.com>
2022-03-27 00:00:05 +0800

    Added setHeader hooks

--- webapp/src/context/HeaderContext.ts
+++ webapp/src/context/HeaderContext.ts
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 
 export interface HeaderFlags {
   showSubjectDropdown: boolean;
+  headerStringValue: string;
 }
 
 export interface HeaderContextValue {
@@ -12,6 +13,7 @@ export interface HeaderContextValue {
 const HeaderContext = createContext<HeaderContextValue>({
   headerFlags: {
     showSubjectDropdown: true,
+    headerStringValue: '',
   },
   setHeaderContextValue: headerFlags => {},
 });

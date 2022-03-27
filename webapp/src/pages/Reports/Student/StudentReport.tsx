1a11612ed1a42c4aac461eba951b3d41fd9cf375
thisdotEric <siguenzajohneric@gmail.com>
2022-03-27 08:57:11 +0800

    Partially implemented student reports pages

--- webapp/src/pages/Reports/Student/StudentReport.tsx
+++ webapp/src/pages/Reports/Student/StudentReport.tsx
@@ -1,15 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useFetch, useSetHeader, useSetPageTitle } from '../../../hooks';
 import './StudentReport.scss';
 
 interface StudentReportProps {}
 
 const StudentReport: FC<StudentReportProps> = ({}: StudentReportProps) => {
+  useSetPageTitle('Student Report');
+
   const { LRN } = useParams();
+
+  const [{ data }, { runFetch: getStudent }] = useFetch(`student/${LRN}`);
+  useSetHeader({
+    headerStringValue: `Individual performance report`,
+    showSubjectDropdown: true,
+  });
+
+  useEffect(() => {
+    getStudent();
+
+    console.log('Current data');
+  }, []);
 
   return (
     <div id="student-report">
-      <p>Reports page for {LRN}</p>
+      {data ? (
+        <>
+          <p id="name">
+            {data.data.last_name}, {data.data.first_name}{' '}
+            {data.data.middle_name}
+          </p>
+        </>
+      ) : (
+        <p>Loading</p>
+      )}
     </div>
   );
 };

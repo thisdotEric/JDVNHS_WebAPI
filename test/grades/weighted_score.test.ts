4ffdba20eea27157b688c873b4b12cf45e84c979
thisdotEric <siguenzajohneric@gmail.com>
2022-04-01 08:51:54 +0800

    Added new test cases for computing weighted score

--- test/grades/weighted_score.test.ts
+++ test/grades/weighted_score.test.ts
@@ -2,35 +2,31 @@ import { computeWeightedScore } from '../../src/algorithms/grades';
 
 describe('Weighted scores unit tests', () => {
   it('should compute for rounded (2 decimal places) weighted score', () => {
-    const percentageScore = 90.63;
-    const weightOfComponent = 0.3;
+    const weightOfComponentForWrittenWork = 0.3;
+    const weightOfComponentForPerformanceTask = 0.5;
 
-    const computedWS = /** 90.63 * 0.3 = */ 27.19;
+    // 90.63 * 0.3 = 27.19;
+    expect(
+      computeWeightedScore(90.63, weightOfComponentForWrittenWork)
+    ).toEqual(27.19);
 
-    const actual = computeWeightedScore(percentageScore, weightOfComponent);
-    expect(actual).toEqual(computedWS);
+    // 83.33 * 0.5 = 41.67
+    expect(
+      computeWeightedScore(83.33, weightOfComponentForPerformanceTask)
+    ).toEqual(41.67);
   });
 
   it('should throw an error when the total raw score or weight of component is a negative number', () => {
     expect(() => {
-      computeWeightedScore(89, -30);
+      computeWeightedScore(89, -0.3);
     }).toThrowError(
       'Total raw score or weight of component should be an unsigned number'
     );
 
     expect(() => {
-      computeWeightedScore(-89, 20);
+      computeWeightedScore(-89, 0.3);
     }).toThrowError(
       'Total raw score or weight of component should be an unsigned number'
     );
   });
-
-  // it('should throw an error if the total raw score or highest possible score is not an integer', () => {
-  //   const totalRawScore = 100.22;
-  //   const highestPossibleScore = 160.23;
-
-  //   expect(() => {
-  //     computePercentageScore(totalRawScore, highestPossibleScore);
-  //   }).toThrowError('Scores should be a whole number');
-  // });
 });

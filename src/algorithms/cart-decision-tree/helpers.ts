aee2bc71fbf09c58de47936bdf1783ee35be7a4d
thisdotEric <siguenzajohneric@gmail.com>
2022-04-16 00:34:46 +0800

    Fixed gini index formula implementation

--- src/algorithms/cart-decision-tree/helpers.ts
+++ src/algorithms/cart-decision-tree/helpers.ts
@@ -13,10 +13,31 @@ export interface ClassCount {
 export const countClass = (rows: StudentAttributes[]): ClassCount => {
   const classCounts: ClassCount = { falseCount: 0, trueCount: 0 };
 
+  /**
+   * For this dataset the only labels are true or false
+   */
   for (let { conduct_intervention } of rows) {
     if (conduct_intervention) classCounts.trueCount++;
     else classCounts.falseCount++;
   }
 
   return classCounts;
+};
+
+/**
+ * Computes the gini impurity of the given dataset
+ * @param rows - Array of StudentAttriburtes interface
+ * @returns number - Gini impurity value
+ */
+export const computeGiniImpurity = (rows: StudentAttributes[]): number => {
+  const { falseCount, trueCount } = countClass(rows);
+  const rowLength = rows.length;
+
+  const true_lbl_probability = trueCount / rowLength;
+  const false_lbl_probability = falseCount / rowLength;
+
+  const gini_impurity =
+    1 - Math.pow(true_lbl_probability, 2) - Math.pow(false_lbl_probability, 2);
+
+  return gini_impurity;
 };
--- test/cart-decision-tree/helpers.test.ts
+++ test/cart-decision-tree/helpers.test.ts
@@ -1,41 +1,61 @@
 import {
   countClass,
   ClassCount,
+  computeGiniImpurity,
+  StudentAttributes,
 } from '../../src/algorithms/cart-decision-tree';
+
+const testData: StudentAttributes[] = [
+  {
+    gender: 'male',
+    grading_period: 1,
+    passedPreTest: false,
+    pt_wScore: 50,
+    qa_wScore: 55,
+    ww_wScore: 56,
+    conduct_intervention: true,
+  },
+  {
+    gender: 'female',
+    grading_period: 1,
+    passedPreTest: false,
+    pt_wScore: 50,
+    qa_wScore: 55,
+    ww_wScore: 56,
+    conduct_intervention: true,
+  },
+  {
+    gender: 'female',
+    grading_period: 1,
+    passedPreTest: false,
+    pt_wScore: 50,
+    qa_wScore: 55,
+    ww_wScore: 56,
+    conduct_intervention: false,
+  },
+  {
+    gender: 'female',
+    grading_period: 1,
+    passedPreTest: false,
+    pt_wScore: 50,
+    qa_wScore: 55,
+    ww_wScore: 56,
+    conduct_intervention: false,
+  },
+];
 
 describe('CART helper methods', () => {
   it('should return an object containing the number of `true` and `false` rows', () => {
-    const expected: ClassCount = { trueCount: 2, falseCount: 1 };
-    const actual = countClass([
-      {
-        gender: 'male',
-        grading_period: 1,
-        passedPreTest: false,
-        pt_wScore: 50,
-        qa_wScore: 55,
-        ww_wScore: 56,
-        conduct_intervention: true,
-      },
-      {
-        gender: 'female',
-        grading_period: 1,
-        passedPreTest: false,
-        pt_wScore: 50,
-        qa_wScore: 55,
-        ww_wScore: 56,
-        conduct_intervention: true,
-      },
-      {
-        gender: 'female',
-        grading_period: 1,
-        passedPreTest: false,
-        pt_wScore: 50,
-        qa_wScore: 55,
-        ww_wScore: 56,
-        conduct_intervention: false,
-      },
-    ]);
+    const expected: ClassCount = { trueCount: 2, falseCount: 2 };
+    const actual = countClass(testData);
 
     expect(actual).toStrictEqual(expected);
+  });
+
+  it('should return the gini impurity value of a given dataset rows', () => {
+    const gini_impurity = 0.5;
+    const actual = computeGiniImpurity(testData);
+
+    expect(actual).toBe(gini_impurity);
   });
 });
